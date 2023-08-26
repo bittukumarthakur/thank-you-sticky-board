bf50264561f4b9c16228300b591140585a099140
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-26 14:42:56 +0530

    initial commit, project setup

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2019": true
+  },
+
+  parserOptions: {
+    ecmaVersion: "latest"
+  },
+
+  "ignorePatterns": [".eslintrc.js"],
+
+  rules: {
+    "semi": ["error", "always"],
+    "camelcase": ["error"],
+    "id-length": ["warn", { exceptions: ["_"], min: 2, max: 25 }],
+    "prefer-const": ["error"],
+    "valid-typeof": ["error"],
+
+    "no-new": ["error"],
+    "no-undef-init": ["error"],
+    "no-irregular-whitespace": ["error"],
+    "no-eval": ["error"],
+
+    "max-classes-per-file": ["error", 3],
+    "max-params": ["warn", 4],
+    "max-lines-per-function": ["error", 25],
+    "max-depth": ["error", 3]
+  }
+}
+
