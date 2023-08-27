cff7595d96f42a736cef1c7912531ffc537d722a
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-27 10:40:34 +0530

    add a handler that authenticate user

---
+++ src/middleware/auth-user.js
@@ -0,0 +1,13 @@
+const authUser = (request, response, next) => {
+  const authToken = request.cookie["auth-token"];
+
+  if (!authToken) {
+    response.redirect("/login");
+    response.send();
+    return;
+  }
+
+  next();
+};
+
+module.exports = { authUser };
