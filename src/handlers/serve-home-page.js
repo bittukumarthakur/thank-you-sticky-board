634b5b1fac93024e3c4bcac7a192c7c91982063e
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-27 22:18:12 +0530

    refactor serve home page handler

---
+++ src/handlers/serve-home-page.js
@@ -0,0 +1,25 @@
+const { readFile } = require("node:fs");
+
+const createNotesElement = (notesDetails) => {
+  return notesDetails.map(({ username, message }) => {
+    return `<div class="note">
+     <h2 class="name">${username}</h2>
+     <div class="message"><p>${message}</p></div>
+    </div>`;
+  }).join("\n");
+};
+
+const serveHomepage = (request, response) => {
+  const { board, auth } = request.app.context;
+  const authToken = request.cookie["auth-token"];
+  const username = auth.getUserInfo(authToken);
+  const notesDetails = board.getNotes();
+  const notesElement = createNotesElement(notesDetails);
+
+  readFile("./public/index.html", "utf-8", (err, template) => {
+    const homePageHtml = template.replace("--notes", notesElement);
+    response.send(homePageHtml);
+  });
+};
+
+module.exports = { serveHomepage };
