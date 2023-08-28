79d8c581e0dcc1c3a37c12499018b0769f2f7971
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-28 17:48:33 +0530

    add end-poinst test for create-app-test

--- test/app/create-app-test.js
+++ test/app/create-app-test.js
@@ -1,19 +1,63 @@
+/* eslint-disable max-lines-per-function */
 const { it, describe } = require("node:test");
 const request = require("supertest");
 const { createApp } = require("../../src/app/create-app");
+const Authenticate = require("../../src/models/authenticate");
+const { authUser } = require("../../src/middleware/auth-user");
+const { parseCookie } = require("../../src/middleware/parseCookie");
 
 describe("app", () => {
 
-  describe("GET /", () => {
-    it.skip("should serve home page", (_, done) => {
-      const app = createApp();
+  describe("GET /login", () => {
+    it("should serve the login page", (_, done) => {
+      const auth = new Authenticate();
+      const app = createApp(auth, authUser, parseCookie);
 
       request(app)
-        .get("/")
+        .get("/login")
         .expect("content-type", /text\/html/)
         .expect(200)
         .end(done);
     });
-
   });
+
+  describe("POST /login", () => {
+    it("should post the login details, redirect to home-page", (_, done) => {
+      const auth = new Authenticate();
+      const app = createApp(auth, authUser, parseCookie);
+
+      request(app)
+        .post("/login")
+        .type("application/x-www-form-urlencoded")
+        .send("username=bittu&password=123")
+        .expect(302)
+        .expect("content-type", /text\/plain/)
+        .expect("set-cookie", "auth-token=bittu-123; Path=/")
+        .expect("location", "/")
+        .end(done);
+    });
+  });
+
+  describe("GET /", () => {
+    it("should serve home page to logged_in user", (_, done) => {
+      const auth = new Authenticate();
+      const app = createApp(auth, authUser, parseCookie);
+
+      request(app)
+        .post("/login")
+        .type("application/x-www-form-urlencoded")
+        .send("username=bittu&password=123")
+        .expect("set-cookie", "auth-token=bittu-123; Path=/")
+        .end(() => {
+          request(app)
+            .get("/")
+            .set("Cookie", "auth-token=bittu-123")
+            .expect(200)
+            .expect("content-type", /text\/html/)
+            .end(done);
+        });
+    });
+  });
+
+
 });
